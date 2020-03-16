4927c445d6390deb99807314f1c2dccbebf72a2c
Willian Krueger <wkrueger128@gmail.com>
2020-03-16 17:53:51 -0300

    remove built from git (1)

--- built/main.d.ts
+++
@@ -1,5 +0,0 @@
-import { genTypes as genTypes_ } from './gen-types';
-export declare const genTypes: typeof genTypes_;
-export declare const merge: typeof genTypes_;
-import { genPaths as genPaths_ } from './gen-paths';
-export declare const genPaths: typeof genPaths_;
--- built/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const gen_types_1 = require("./gen-types");
-exports.genTypes = gen_types_1.genTypes;
-exports.merge = gen_types_1.genTypes;
-const gen_paths_1 = require("./gen-paths");
-exports.genPaths = gen_paths_1.genPaths;
-//# sourceMappingURL=main.js.map
