d8db55a8c36de7fb8a256ae8e40893eb283218e5
Willian Krueger <wkrueger128@gmail.com>
2020-03-17 15:20:50 -0300

    open api v3 body

--- test/test.js
+++ test/test.js
@@ -7,36 +7,36 @@ const nestjs = require("./nestjs.json")
 const path = require("path")
 
 async function run() {
-  await tap.test("sendgrid", async t => {
-    await tstemplate.genPaths(sendgrid, {
-      output: path.resolve(__dirname, "output", "sendgrid"),
-      typesOpts: { hideComments: true },
-      moduleStyle: "esm"
-    })
-  })
+  // await tap.test("sendgrid", async t => {
+  //   await tstemplate.genPaths(sendgrid, {
+  //     output: path.resolve(__dirname, "output", "sendgrid"),
+  //     typesOpts: { hideComments: true },
+  //     moduleStyle: "esm"
+  //   })
+  // })
 
-  await tap.test("uber", async t => {
-    await tstemplate.genPaths(uber, {
-      output: path.resolve(__dirname, "output", "uber"),
-      typesOpts: { hideComments: true },
-      moduleStyle: "esm"
-    })
-  })
+  // await tap.test("uber", async t => {
+  //   await tstemplate.genPaths(uber, {
+  //     output: path.resolve(__dirname, "output", "uber"),
+  //     typesOpts: { hideComments: true },
+  //     moduleStyle: "esm"
+  //   })
+  // })
 
-  await tap.test("spring", async t => {
-    await tstemplate.genPaths(spring, {
-      output: path.resolve(__dirname, "output", "spring"),
-      typesOpts: { hideComments: true },
-      moduleStyle: "esm"
-    })
-  })
+  // await tap.test("spring", async t => {
+  //   await tstemplate.genPaths(spring, {
+  //     output: path.resolve(__dirname, "output", "spring"),
+  //     typesOpts: { hideComments: true },
+  //     moduleStyle: "esm"
+  //   })
+  // })
 
-  await tap.test("empty", async t => {
-    await tstemplate.genPaths(null, {
-      output: path.resolve(__dirname, "output", "empty"),
-      moduleStyle: "esm"
-    })
-  })
+  // await tap.test("empty", async t => {
+  //   await tstemplate.genPaths(null, {
+  //     output: path.resolve(__dirname, "output", "empty"),
+  //     moduleStyle: "esm"
+  //   })
+  // })
 
   await tap.test("nestjs", async t => {
     await tstemplate.genPaths(nestjs, {
